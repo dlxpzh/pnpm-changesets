2640639afb08101aa8cd13df1354168bb4a2026f
谢忠平 <xiezhongping@fadada.com>
2023-03-09 10:47:37 +0800

    fix: changeset

--- packages/plugin/src/App.vue
+++ packages/plugin/src/App.vue
@@ -11,7 +11,7 @@ import HelloWorld from "./components/HelloWorld.vue";
             <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
         </a>
     </div>
-    <HelloWorld msg="monorepo + plugin" />
+    <HelloWorld msg="monorepo + plugin插件" />
 </template>
 
 <style scoped>
--- packages/ui/src/App.vue
+++ packages/ui/src/App.vue
@@ -11,7 +11,7 @@ import HelloWorld from "./components/HelloWorld.vue";
             <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
         </a>
     </div>
-    <HelloWorld msg="monorepo + ui" />
+    <HelloWorld msg="monorepo + ui组件" />
 </template>
 
 <style scoped>
