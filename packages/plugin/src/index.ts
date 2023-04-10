ed310daa381040dc90b3ab975675d29be226c48a
谢忠平 <xiezhongping@fadada.com>
2023-04-10 14:19:57 +0800

    docs(changeset): add file · d:\dlxpzh-project\pnpm-changesets\packages\plugin\src\index.ts: 1 add file · d:\dlxpzh-project\pnpm-changesets\packages\ui\src\App.vue: 2 add file · d:\dlxpzh-project\pnpm-changesets\packages\ui\src\main.ts: 3 add file · d:\dlxpzh-project\pnpm-changesets\packages\ui\src\style.css: 4 add file · d:\dlxpzh-project\pnpm-changesets\packages\ui\src\vite-env.d.ts: 5 11111

--- packages/plugin/src/index.ts
+++ packages/plugin/src/index.ts
@@ -1,5 +1,5 @@
 const test = () => {
-    return "pnpm-plugin：Test turboRepo project changelog 日志";
+    return "pnpm-plugin：Test turboRepo project changelog 日志 自定义changeset";
 };
 
 export default test;
